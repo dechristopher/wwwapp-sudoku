3ae4d58f3e28e2879c064d9ecf51c70d6fd79448
Andrew DeChristopher <andrewdechristopher@gmail.com>
2018-03-30 09:01:03 -0400

    Began implementing authentication endpoints and finished solve endpoint

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const app = express();
 const https = require('https');
 const timeout = require('express-timeout-handler');
 const cookieParser = require('cookie-parser');
+const hasher = require('password-hash-and-salt');
 
 // Core Node Modules
 const fs = require('fs');
@@ -41,6 +42,9 @@ const strInitShutdown = 'Init service shutdown';
 // HTTP server variable
 let srv;
 
+// DB Connection
+let dbc;
+
 // Set up timeout middleware options
 const timeoutOptions = {
 	timeout: conf.timeout,
@@ -56,13 +60,14 @@ const timeoutOptions = {
 
 // Set up session options
 const sess = {
-	secret: conf.authToken,
+	secret: conf.secret,
 	cookie: {},
 };
 
 // Ensure requests time out after 2 seconds
 app.use(timeout.handler(timeoutOptions));
 
+// Parse user cookies
 app.use(cookieParser());
 
 // Use the session middleware
@@ -76,6 +81,7 @@ app.use(bodyParser.urlencoded({
 	extended: true,
 }));
 
+// Serve HTML resources statically
 app.use(express.static('templates/assets'));
 
 // Run the homepage template
@@ -122,14 +128,52 @@ app.get('/leaderboard', (req, res) => {
 
 // Log in and redirect to home
 app.post('/login', (req, res) => {
-	// TODO
-	let success;
+	// Ensure all credentials are provided
+	if(!req.body.username) {
+		log('no username');
+		res.sendStatus(400);
+		return;
+	}
+	if(!req.body.psw) {
+		log('no password');
+		res.sendStatus(400);
+		return;
+	}
+
 	// DO DB hit and verify credentials
-	req.session.username = req.body.username;
-	res.cookie('sid', req.session.id);
-	res.cookie('username', req.session.username);
-	log(`[POST /login] (LOGIN ${req.body.username} [${success}]) ( ${req.ip} )`);
+	dbc.query('SELECT `username` FROM `user` WHERE `username` = ?', [req.body.username], function(error, results) {
+		if (error) {
+			log(`[GET USER EXISTS ERROR] ${error}`);
+			return;
+		}
+
+		if(!results.length) {
+			log(`[POST /login] (LOGIN FAIL [user nonexistant] ${req.body.username}) ( ${req.ip} )`);
+			res.redirect('/?e=badLogin');
+			return;
+		}
+
+		verifyPassword(req.body.username, req.body.psw).then(validated => {
+			if(!validated) {
+				log(`[POST /login] (LOGIN FAIL [bad password] ${req.body.username}) ( ${req.ip} )`);
+				res.redirect('/?e=badLogin');
+				return;
+			}
+			req.session.username = req.body.username;
+			res.cookie('sid', req.session.id);
+			res.cookie('username', req.session.username);
+			log(`[POST /login] (LOGIN ${req.body.username}) ( ${req.ip} )`);
+			res.redirect('/');
+		});
+	});
+});
+
+
+app.post('/register', (req, res) => {
+	log();
 	res.redirect('/');
+
+	// TODO REGISTER
 });
 
 // Log out and redirect to home
@@ -149,9 +193,65 @@ app.get('/logout', (req, res) => {
 
 // AJAX, no user redirection or HTML parsing needed
 app.post('/solve', (req, res) => {
+	const userID = req.cookies.userID;
+	const time = req.body.time;
+	const difficulty = req.body.difficulty;
+	const seed = req.body.seed;
+	log(`[POST /solve] (USERNAME: ${req.session.username}, UID: ${userID}, SOLVED IN: ${time}sec, DIFF:${difficulty}) (${req.ip})`);
 	// Post times to DB
-	res.sendStatus(200);
+	dbc.query('INSERT INTO `solves` (user_id, time, seed, difficulty_id) VALUES (?, ?, ?, ?)', [userID, time, seed, difficulty], function(error) {
+		if(error) {
+			log(`[POST SOLVE ERROR] ${error}`);
+			res.sendStatus(500);
+			return;
+		}
+		res.sendStatus(200);
+	});
 });
+
+/**
+ * Generates a crpytographically secure hash with BCrypt and salts it properly
+ * @param {string} secret the password to hash
+ * @returns {Promise<string>} the hash
+ */
+function genHash(secret) {
+	return new Promise(function(resolve, reject) {
+		hasher(secret).hash(function(error, hash) {
+			if (error) {
+				reject();
+				throw new Error('Something went wrong!');
+			}
+			resolve(hash);
+		});
+	});
+}
+/**
+ * Verifies that a given secret matches the stored hash for user auth
+ * @param {*} username the user's username
+ * @param {*} secret the user's password
+ * @returns {Promise<boolean>} validated or not
+ */
+function verifyPassword(username, secret) {
+	return new Promise(function(resolve) {
+		dbc.query('SELECT `password` FROM `user` WHERE `username` = ?', [username], function(error, results) {
+			if (error) {
+				log(`[GET PASSWORD HASH ERROR] ${error}`);
+				return;
+			}
+
+			hasher(secret).verifyAgainst('p' + results[0].password, function(errorV, verified) {
+				if (errorV) {
+					resolve(false);
+					throw new Error(`[VERIFY PASSWORD HASH ERROR] ${errorV}`);
+				}
+				if (!verified) {
+					resolve(false);
+				}
+				resolve(true);
+			});
+		});
+	});
+}
 
 function init() {
 	// SSL Mode Logic
@@ -192,6 +292,7 @@ function terminate() {
 		log(strInitShutdown, { newLinePre: true }).then(() => {
 			log(`${os.EOL}~${os.EOL}${os.EOL}`, { stdOut: false, usePrefix: false }).then(() => {
 				// Close db, cache connections, etc...
+				dbc.destroy();
 				// Terminate with exit code 0
 				process.exit(0);
 			});
@@ -213,5 +314,7 @@ process.on('SIGINT', function() {
 ascii().then(console.log).then(() => {
 	log(strServiceInit);
 	init();
+	dbc = new (require('./modules/services/dbc'))(conf.db).conn();
+	// genHash('memes').then(console.log);
 });
 
